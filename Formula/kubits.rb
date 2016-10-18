b315d4f38f10d78c71d301920470b26d5767857a
joe miller <joeym@joeym.net>
2016-10-18 12:16:12 -0700

    bump to 0.0.7

--- Formula/kubits.rb
+++ Formula/kubits.rb
@@ -1,13 +1,8 @@
 class Kubits < Formula
   desc 'utilities for working kubernetes clusters'
   homepage 'https://github.com/joemiller/kubits'
-  url 'https://github.com/joemiller/kubits.git',
-    using: :git,
-    tag: 'v0.0.6'
-
-  # TODO: when we switch to a public github repo, use https tarball url method instead, example:
-  #   url 'https://github.com/joemiller/kubits/archive/v0.0.5.tar.gz'
-  #   sha256 '...checksum here...'
+  url 'https://github.com/joemiller/kubits/archive/v0.0.7.tar.gz'
+  sha256 '73322e477cae0add3b38f70eb8c619b9bb578f9e7dfdd0c22bdedfb8cbede754'
 
   head 'https://github.com/joemiller/kubits.git'
 
